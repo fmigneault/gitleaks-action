c3d06748e261b2cc7439aa551e26222b35418129
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-04-14 16:17:14 -0400

    log gitleaks call args

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,33 +13,38 @@ echo "GITHUB_BASE_REF:   ${GITHUB_BASE_REF}"
 cd "$GITHUB_WORKSPACE"
 git fetch --quiet
 
-if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]] 
+if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]
 then
     # scan only new commits contained within the PR
     GITHUB_HEAD_SHA=$(git rev-parse "origin/${GITHUB_HEAD_REF}")
+
+    (set -o xtrace;
     gitleaks -v --exclude-forks --redact --threads=1 \
-      --commit-to=$GITHUB_HEAD_SHA \
-      --commit-from=$GITHUB_BASE_REF \
-      --repo-path=$GITHUB_WORKSPACE
+      --commit-to=${GITHUB_HEAD_SHA} \
+      --commit-from=${GITHUB_BASE_REF} \
+      --repo-path=${GITHUB_WORKSPACE}
+    )
 else
     # branch/tag name in the form "refs/<ref-type>/<ref-id>[/<ref-subtype>]"
     # ref-type: heads|pull|tags
-    GITHUB_REF_NAME="$(echo $GITHUB_REF | cut -d '/' -f3)"
+    GITHUB_REF_NAME="$(echo ${GITHUB_REF} | cut -d '/' -f3)"
     # run only from current to master instead of full history
     GITHUB_REF_MASTER=$(git rev-parse 'origin/master')
-    
-    if [[ "${GITHUB_REF_NAME}" == "master" ]] 
+
+    (set -o xtrace;
+    if [[ "${GITHUB_REF_NAME}" == "master" ]]
     then
         # push to master (eg: for tagged versioning), only scan the last commit
         gitleaks -v --exclude-forks --redact --threads=1 \
           --branch=$GITHUB_REF_NAME \
           --depth=1 \
           --repo-path=$GITHUB_WORKSPACE
-    else        
+    else
         gitleaks -v --exclude-forks --redact --threads=1 \
           --branch=$GITHUB_REF_NAME \
           --commit-to=$GITHUB_REF_MASTER \
           --repo-path=$GITHUB_WORKSPACE
     fi
+    )
 fi
 
