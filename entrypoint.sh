f7d5755bb7b04046c2c7bd29d4c4b7a57a38b0e2
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-04-15 22:06:49 -0400

    adjust inspected pr-merge refs on merge trigger to only scan pr commits

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,12 +17,27 @@ if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]
 then
     # scan only new commits contained within the PR
     GITHUB_HEAD_SHA=$(git rev-parse "origin/${GITHUB_HEAD_REF}")
+    GITHUB_BASE_SHA=$(git rev-parse "origin/${GITHUB_BASE_REF}")
 
+    # when PR refers to a merge commit, we need to tweak values
+    # scan github reference for final 'merge' in 'refs/pull/<pr-id>/merge'
+    GITHUB_REF_NAME="$(echo ${GITHUB_REF} | cut -d '/' -f4)"
     (set -o xtrace;
-    gitleaks -v --exclude-forks --redact --threads=1 \
-      --commit-to=${GITHUB_HEAD_SHA} \
-      --commit-from=${GITHUB_BASE_REF} \
-      --repo-path=${GITHUB_WORKSPACE}
+    if [[ "${GITHUB_REF_NAME}" == "merge" ]]
+    then
+        # 'merge' PR trigger
+        # commits between source and destination branches are inverted (flip commit-to/from)
+        gitleaks -v --exclude-forks --redact --threads=1 \
+            --commit-from=${GITHUB_HEAD_SHA} \
+            --commit-to=${GITHUB_BASE_SHA} \
+            --repo-path=/repo
+    else
+        # 'normal' PR trigger, such as when the job to test new commits is re-run
+        gitleaks -v --exclude-forks --redact --threads=1 \
+          --commit-to=${GITHUB_HEAD_SHA} \
+          --commit-from=${GITHUB_BASE_SHA} \
+          --repo-path=${GITHUB_WORKSPACE}
+    fi
     )
 else
     # branch/tag name in the form "refs/<ref-type>/<ref-id>[/<ref-subtype>]"
@@ -36,14 +51,14 @@ else
     then
         # push to master (eg: for tagged versioning), only scan the last commit
         gitleaks -v --exclude-forks --redact --threads=1 \
-          --branch=$GITHUB_REF_NAME \
+          --branch=${GITHUB_REF_NAME} \
           --depth=1 \
-          --repo-path=$GITHUB_WORKSPACE
+          --repo-path=${GITHUB_WORKSPACE}
     else
         gitleaks -v --exclude-forks --redact --threads=1 \
-          --branch=$GITHUB_REF_NAME \
-          --commit-to=$GITHUB_REF_MASTER \
-          --repo-path=$GITHUB_WORKSPACE
+          --branch=${GITHUB_REF_NAME} \
+          --commit-to=${GITHUB_REF_MASTER} \
+          --repo-path=${GITHUB_WORKSPACE}
     fi
     )
 fi
